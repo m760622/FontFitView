61d8759358fc75ec6163b28c1dd21f421c282446
Mohammed Abunada <m7606225@gmail.com>
2021-01-24 21:46:41 +0100

    Initial Commit

---
+++ FontFitView/FontFitViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontFitViewApp.swift
+//  FontFitView
+//
+//  Created by Mohammed Abunada on 2021-01-24.
+//
+
+import SwiftUI
+
+@main
+struct FontFitViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
